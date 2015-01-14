f4556aab0a3139a3cbdd9579e049b5c06b0ae3e0
Hiroyuki Sano <sh19910711@gmail.com>
2015-01-14 19:42:22 +0900

    test: change namespace

--- spec/webapi/controllers/v0/repositories_controller_spec.rb
+++ spec/webapi/controllers/v0/repositories_controller_spec.rb
@@ -1,232 +1,236 @@
 require "spec_helper"
 
-describe SocialSnippet::Registry::WebAPI::WebAPIv0 do
+module SocialSnippet::Registry::WebAPI
 
-  describe "repository_controller" do
+  describe WebAPIv0 do
 
-    app SocialSnippet::Registry::WebAPI::WebAPIv0
+    describe "repository_controller" do
 
-    let(:api_version) { '/v0' }
+      app WebAPIv0
 
-    context "add my-repo" do
+      let(:api_version) { '/v0' }
 
-      before do
-        FactoryGirl.define do
-          factory "my-repo", :class => Repository do
-            name "my-repo"
-            desc "This is my repository"
-            url "git://github.com/user/my-repo.git"
-          end 
-        end # define my-repo
-
-        FactoryGirl.create "my-repo"
-      end # prepare my-repo
-
-      context "get repositories/my-repo" do
-
-        before { get "#{api_version}/repositories/my-repo" }
-        it { expect(last_response).to be_ok }
-
-        context "check result" do
-          let(:result) { JSON.parse(last_response.body) }
-          it { expect(result["name"]).to eq "my-repo" }
-          it { expect(result["url"]).to eq "git://github.com/user/my-repo.git" }
-          it { expect(result["desc"]).to eq "This is my repository" }
-          it { expect(result["dependencies"]).to be_empty }
-        end
-
-      end
-
-      context "get repositories" do
+      context "add my-repo" do
 
         before do
-          get "#{api_version}/repositories"
+          FactoryGirl.define do
+            factory "my-repo", :class => Repository do
+              name "my-repo"
+              desc "This is my repository"
+              url "git://github.com/user/my-repo.git"
+            end
+          end # define my-repo
+
+          FactoryGirl.create "my-repo"
+        end # prepare my-repo
+
+        context "get repositories/my-repo" do
+
+          before { get "#{api_version}/repositories/my-repo" }
+          it { expect(last_response).to be_ok }
+
+          context "check result" do
+            let(:result) { JSON.parse(last_response.body) }
+            it { expect(result["name"]).to eq "my-repo" }
+            it { expect(result["url"]).to eq "git://github.com/user/my-repo.git" }
+            it { expect(result["desc"]).to eq "This is my repository" }
+            it { expect(result["dependencies"]).to be_empty }
+          end
+
         end
 
-        it { expect(last_response).to be_ok }
-
-        context "check result json" do
-          let(:result) { JSON.parse(last_response.body) }
-          it { expect(result.length).to eq 1 }
-          it { expect(result[0]["name"]).to eq "my-repo" }
-          it { expect(result[0]["url"]).to eq "git://github.com/user/my-repo.git" }
-          it { expect(result[0]["desc"]).to eq "This is my repository" }
-        end # check result json
-
-      end # get repositories
-
-      context "add new-repo" do
-
-        context "post repositories" do
-
-          let(:new_repo_url_raw) { "git://github.com/user/new-repo" }
-          let(:new_repo_url) { URI.encode_www_form_component new_repo_url_raw }
+        context "get repositories" do
 
           before do
-            allow_any_instance_of(::SocialSnippet::Registry::WebAPI::WebAPIv0).to receive(:create_fetcher) do
-              class Dummy; end
-              dummy = Dummy.new
-              allow(dummy).to receive(:snippet_json) do |_|
-                {
-                  "name" => "new-repo",
-                  "desc" => "This is new repo",
-                  "language" => "C++",
-                  "main" => "src",
-                  "dependencies" => {
-                    "my-repo" => "1.0.0",
-                  },
-                }
-              end
-              dummy
-            end
-          end # clone dummy repo
+            get "#{api_version}/repositories"
+          end
 
-          context "post by json params" do
+          it { expect(last_response).to be_ok }
 
-            before do
-              # TODO: check without token
-              header "X-CSRF-TOKEN", "dummy-token"
-              data = {
-                "url" => new_repo_url_raw,
-              }
-              post(
-                "#{api_version}/repositories",
-                data.to_json,
-                {
-                  "CONTENT_TYPE" => "application/json",
-                  "rack.session" => {
-                    :csrf => "dummy-token",
-                  },
-                },
-              )
-            end
+          context "check result json" do
+            let(:result) { JSON.parse(last_response.body) }
+            it { expect(result.length).to eq 1 }
+            it { expect(result[0]["name"]).to eq "my-repo" }
+            it { expect(result[0]["url"]).to eq "git://github.com/user/my-repo.git" }
+            it { expect(result[0]["desc"]).to eq "This is my repository" }
+          end # check result json
 
-            it "", :current => true do; end # TODO: remove
+        end # get repositories
 
-            it { expect(last_response).to be_ok }
+        context "add new-repo" do
 
-            context "check result model" do
+          context "post repositories" do
 
-              let(:repo) { Repository.find_by(:name => "new-repo") }
-              it { expect(repo.name).to eq "new-repo" }
-              it { expect(repo.url).to eq "git://github.com/user/new-repo.git" }
-
-            end # check result model
-
-            context "with query" do
-
-              context "get repositories?q=new" do
-
-                before { get "#{api_version}/repositories?q=new" }
-
-                it { expect(last_response).to be_ok }
-
-                context "check result json" do
-                  let(:result) { JSON.parse(last_response.body) }
-                  it { expect(result.length).to eq 1 }
-                  it { expect(result[0]["name"]).to eq "new-repo" }
-                  it { expect(result[0]["url"]).to eq "git://github.com/user/new-repo.git" }
-                  it { expect(result[0]["desc"]).to eq "This is new repo" }
-                end
-
-              end # get repositories?q=new
-
-              context "get repositories?q=my" do
-
-                before { get "#{api_version}/repositories?q=my" }
-
-                it { expect(last_response).to be_ok }
-
-                context "check result json" do
-                  let(:result) { JSON.parse(last_response.body) }
-                  it { expect(result.length).to eq 1 }
-                  it { expect(result[0]["name"]).to eq "my-repo" }
-                  it { expect(result[0]["url"]).to eq "git://github.com/user/my-repo.git" }
-                  it { expect(result[0]["desc"]).to eq "This is my repository" }
-                end
-
-              end # get repositories?q=new
-
-            end # with query
-
-          end # post by query params
-
-          context "post by query params" do
+            let(:new_repo_url_raw) { "git://github.com/user/new-repo" }
+            let(:new_repo_url) { URI.encode_www_form_component new_repo_url_raw }
 
             before do
-              # TODO: check without token
-              header "X-CSRF-TOKEN", "dummy-token"
-              post(
-                "#{api_version}/repositories?url=#{new_repo_url}",
-                {
-                },
-                {
-                  "rack.session" => {
-                    :csrf => "dummy-token",
-                  },
+              allow_any_instance_of(WebAPIv0).to receive(:create_fetcher) do
+                class Dummy; end
+                dummy = Dummy.new
+                allow(dummy).to receive(:snippet_json) do |_|
+                  {
+                    "name" => "new-repo",
+                    "desc" => "This is new repo",
+                    "language" => "C++",
+                    "main" => "src",
+                    "dependencies" => {
+                      "my-repo" => "1.0.0",
+                    },
+                  }
+                end
+                dummy
+              end
+            end # clone dummy repo
+
+            context "post by json params" do
+
+              before do
+                # TODO: check without token
+                header "X-CSRF-TOKEN", "dummy-token"
+                data = {
+                  "url" => new_repo_url_raw,
                 }
-              )
-            end
-
-            it { expect(last_response).to be_ok }
-
-            context "check result model" do
-
-              let(:repo) do
-                Repository.find_by(:name => "new-repo")
+                post(
+                  "#{api_version}/repositories",
+                  data.to_json,
+                  {
+                    "CONTENT_TYPE" => "application/json",
+                    "rack.session" => {
+                      :csrf => "dummy-token",
+                    },
+                  },
+                )
               end
 
-              it { expect(repo.name).to eq "new-repo" }
-              it { expect(repo.url).to eq "git://github.com/user/new-repo.git" }
-              it { expect(repo.dependencies).to_not be_empty }
-              it { expect(repo.dependencies.length).to eq 1 }
+              it "", :current => true do; end # TODO: remove
 
-            end # check result model
+              it { expect(last_response).to be_ok }
 
-            context "with query" do
+              context "check result model" do
 
-              context "get repositories?q=new" do
+                let(:repo) { Repository.find_by(:name => "new-repo") }
+                it { expect(repo.name).to eq "new-repo" }
+                it { expect(repo.url).to eq "git://github.com/user/new-repo.git" }
 
-                before { get "#{api_version}/repositories?q=new" }
+              end # check result model
 
-                it { expect(last_response).to be_ok }
+              context "with query" do
 
-                context "check result json" do
-                  let(:result) { JSON.parse(last_response.body) }
-                  it { expect(result.length).to eq 1 }
-                  it { expect(result[0]["name"]).to eq "new-repo" }
-                  it { expect(result[0]["url"]).to eq "git://github.com/user/new-repo.git" }
-                  it { expect(result[0]["desc"]).to eq "This is new repo" }
+                context "get repositories?q=new" do
+
+                  before { get "#{api_version}/repositories?q=new" }
+
+                  it { expect(last_response).to be_ok }
+
+                  context "check result json" do
+                    let(:result) { JSON.parse(last_response.body) }
+                    it { expect(result.length).to eq 1 }
+                    it { expect(result[0]["name"]).to eq "new-repo" }
+                    it { expect(result[0]["url"]).to eq "git://github.com/user/new-repo.git" }
+                    it { expect(result[0]["desc"]).to eq "This is new repo" }
+                  end
+
+                end # get repositories?q=new
+
+                context "get repositories?q=my" do
+
+                  before { get "#{api_version}/repositories?q=my" }
+
+                  it { expect(last_response).to be_ok }
+
+                  context "check result json" do
+                    let(:result) { JSON.parse(last_response.body) }
+                    it { expect(result.length).to eq 1 }
+                    it { expect(result[0]["name"]).to eq "my-repo" }
+                    it { expect(result[0]["url"]).to eq "git://github.com/user/my-repo.git" }
+                    it { expect(result[0]["desc"]).to eq "This is my repository" }
+                  end
+
+                end # get repositories?q=new
+
+              end # with query
+
+            end # post by query params
+
+            context "post by query params" do
+
+              before do
+                # TODO: check without token
+                header "X-CSRF-TOKEN", "dummy-token"
+                post(
+                  "#{api_version}/repositories?url=#{new_repo_url}",
+                  {
+                  },
+                  {
+                    "rack.session" => {
+                      :csrf => "dummy-token",
+                    },
+                  }
+                )
+              end
+
+              it { expect(last_response).to be_ok }
+
+              context "check result model" do
+
+                let(:repo) do
+                  Repository.find_by(:name => "new-repo")
                 end
 
-              end # get repositories?q=new
+                it { expect(repo.name).to eq "new-repo" }
+                it { expect(repo.url).to eq "git://github.com/user/new-repo.git" }
+                it { expect(repo.dependencies).to_not be_empty }
+                it { expect(repo.dependencies.length).to eq 1 }
 
-              context "get repositories?q=my" do
+              end # check result model
 
-                before { get "#{api_version}/repositories?q=my" }
+              context "with query" do
 
-                it { expect(last_response).to be_ok }
+                context "get repositories?q=new" do
 
-                context "check result json" do
-                  let(:result) { JSON.parse(last_response.body) }
-                  it { expect(result.length).to eq 1 }
-                  it { expect(result[0]["name"]).to eq "my-repo" }
-                  it { expect(result[0]["url"]).to eq "git://github.com/user/my-repo.git" }
-                  it { expect(result[0]["desc"]).to eq "This is my repository" }
-                end
+                  before { get "#{api_version}/repositories?q=new" }
 
-              end # get repositories?q=new
+                  it { expect(last_response).to be_ok }
 
-            end # with query
+                  context "check result json" do
+                    let(:result) { JSON.parse(last_response.body) }
+                    it { expect(result.length).to eq 1 }
+                    it { expect(result[0]["name"]).to eq "new-repo" }
+                    it { expect(result[0]["url"]).to eq "git://github.com/user/new-repo.git" }
+                    it { expect(result[0]["desc"]).to eq "This is new repo" }
+                  end
 
-          end # post by query params
+                end # get repositories?q=new
 
-        end # post repositories
+                context "get repositories?q=my" do
 
-      end # add new-repo
+                  before { get "#{api_version}/repositories?q=my" }
 
-    end # add my-repo
+                  it { expect(last_response).to be_ok }
 
-  end # repositories_controller
+                  context "check result json" do
+                    let(:result) { JSON.parse(last_response.body) }
+                    it { expect(result.length).to eq 1 }
+                    it { expect(result[0]["name"]).to eq "my-repo" }
+                    it { expect(result[0]["url"]).to eq "git://github.com/user/my-repo.git" }
+                    it { expect(result[0]["desc"]).to eq "This is my repository" }
+                  end
 
-end # SocialSnippet::Registry::WebAPI::Versions::WebAPIv0
+                end # get repositories?q=new
+
+              end # with query
+
+            end # post by query params
+
+          end # post repositories
+
+        end # add new-repo
+
+      end # add my-repo
+
+    end # repositories_controller
+
+  end # WebAPIv0
+
+end # SocialSnippet::Registry::WebAPI
--- spec/webapi/models/repository_spec.rb
+++ spec/webapi/models/repository_spec.rb
@@ -1,72 +1,75 @@
 require "spec_helper"
 
-describe Repository do
+module SocialSnippet::Registry::WebAPI
 
-  describe "#to_object" do
+  describe Repository do
 
-    context "create model" do
+    describe "#to_object" do
 
-      let(:model) do
-        Repository.new(
-          :name => "my-repo",
-          :desc => "thisisdesc",
-          :url => "git://url/to/git/repo",
-        )
-      end
+      context "create model" do
 
-      context "call to_object" do
-        let(:result) { model.to_object }
-        it { expect(result[:name]).to eq "my-repo" }
-        it { expect(result[:desc]).to eq "thisisdesc" }
-        it { expect(result[:url]).to  eq "git://url/to/git/repo" }
-      end # call to_object
-
-    end # create model
-
-  end # to_object
-
-  describe "#update_by_snippet_json" do
-
-    context "create model" do
-
-      let(:model) do
-        Repository.new(
-          :name => "my-repo",
-          :desc => "thisisdesc",
-          :url => "git://url/to/git/repo",
-        )
-      end
-
-      context "create repo" do
-
-        let(:json_obj) do
-          {
-            "name" => "new-repo",
-            "desc" => "this is new desc",
-            "dependencies" => {
-              "my-repo" => "1.0.0",
-            },
-          }
+        let(:model) do
+          Repository.new(
+            :name => "my-repo",
+            :desc => "thisisdesc",
+            :url => "git://url/to/git/repo",
+          )
         end
 
-        context "call update_by" do
+        context "call to_object" do
+          let(:result) { model.to_object }
+          it { expect(result[:name]).to eq "my-repo" }
+          it { expect(result[:desc]).to eq "thisisdesc" }
+          it { expect(result[:url]).to  eq "git://url/to/git/repo" }
+        end # call to_object
 
-          before { model.update_by_snippet_json(json_obj) }
+      end # create model
 
-          context "call to_object" do
-            let(:result) { model.to_object }
-            it { expect(result[:name]).to eq "my-repo" }
-            it { expect(result[:desc]).to eq "this is new desc" }
-            it { expect(result[:dependencies]).to include "my-repo" }
+    end # to_object
+
+    describe "#update_by_snippet_json" do
+
+      context "create model" do
+
+        let(:model) do
+          Repository.new(
+            :name => "my-repo",
+            :desc => "thisisdesc",
+            :url => "git://url/to/git/repo",
+          )
+        end
+
+        context "create repo" do
+
+          let(:json_obj) do
+            {
+              "name" => "new-repo",
+              "desc" => "this is new desc",
+              "dependencies" => {
+                "my-repo" => "1.0.0",
+              },
+            }
+          end
+
+          context "call update_by" do
+
+            before { model.update_by_snippet_json(json_obj) }
+
+            context "call to_object" do
+              let(:result) { model.to_object }
+              it { expect(result[:name]).to eq "my-repo" }
+              it { expect(result[:desc]).to eq "this is new desc" }
+              it { expect(result[:dependencies]).to include "my-repo" }
+            end
+
           end
 
         end
 
-      end
+      end # create model
 
-    end # create model
+    end # update_by_real_repo
 
-  end # update_by_real_repo
+  end # Repository
 
-end # Repository
-
+end # SocialSnippet::Registry::WebAPI
