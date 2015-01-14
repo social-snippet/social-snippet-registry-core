5d2e73dd231b7269e2da2d12019a476f334e9e05
Hiroyuki Sano <sh19910711@gmail.com>
2015-01-14 20:21:43 +0900

    user_account: change namespace

--- lib/social_snippet/registry_core/models/user_account.rb
+++ lib/social_snippet/registry_core/models/user_account.rb
@@ -1,7 +1,7 @@
-class SocialSnippet::Registry::WebAPI::UserAccount
+class SocialSnippet::RegistryCore::Models::UserAccount
 
-  include Mongoid::Document
-  include Mongoid::Timestamps # adds created_at and updated_at fields
+  include ::Mongoid::Document
+  include ::Mongoid::Timestamps # adds created_at and updated_at fields
 
   # field <name>, :type => <type>, :default => <value>
   field :name, :type => String
