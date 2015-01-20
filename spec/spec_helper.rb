da4923af4b3c22f6e9eed3b0676e357830f849c1
Hiroyuki Sano <sh19910711@gmail.com>
2015-01-20 17:54:13 +0900

    spec_helper: add codeclimate_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 ENV["RACK_ENV"] = "test"
 
 require "bundler/setup"
+require "spec_helpers/codeclimate_helper"
 require "spec_helpers/rack_test_helper"
 require "spec_helpers/database_cleaner_helper"
 require "spec_helpers/factory_girl_helper"
