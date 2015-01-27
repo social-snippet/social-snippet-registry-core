4906c83011286e4104987a1b9afcf9aa7ada66da
Hiroyuki Sano <sh19910711@gmail.com>
2015-01-27 16:55:13 +0900

    common_helpers: add is_logged_in()

--- lib/social_snippet/registry_core/common_helpers.rb
+++ lib/social_snippet/registry_core/common_helpers.rb
@@ -1,5 +1,10 @@
 module SocialSnippet::RegistryCore::CommonHelpers
 
+  # called from view
+  def is_logged_in
+    logged_in?
+  end
+
   def assets_host
     if ENV["SSPM_ASSETS_HOST"].nil?
       ""
